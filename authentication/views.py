540190afd8e5085ef59832bc4723889c2fc9a13d
PatrickCmd <pwalukagga@gmail.com>
2018-03-23 18:56:12 +0300

    [ft-user-authenticate-register] Add user registration view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .serializers import RegistrationSerializer
+
+
+class RegistrationAPIView(APIView):
+    # Allow any user (authenticated or not) to hit this endpoint.
+    permission_classes = (AllowAny, )
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+        if not user:
+            user = {
+                "email": request.data.get('email'),
+                "username": request.data.get('username'),
+                "password": request.data.get('password')
+            }
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
