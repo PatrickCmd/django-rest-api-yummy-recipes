0ebde6d9133a5ca6ca24ddb9e7c2de69cd52ece3
PatrickCmd <pwalukagga@gmail.com>
2018-03-23 18:55:08 +0300

    [ft-user-authenticate-register] Add django_extensions in installed apps

--- yummy_recipes/settings.py
+++ yummy_recipes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
     'api.apps.ApiConfig',
+    'django_extensions',
     'rest_framework',
 ]
 
