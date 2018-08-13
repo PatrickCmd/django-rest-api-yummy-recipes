ae6d8f5b7dfa00135b08bccc13894eb15733fd6d
PatrickCmd <pwalukagga@gmail.com>
2018-08-13 20:36:21 +0300

    [Feature recipe-reviews-and-upvotes] Implement recipe reviews and upvotes

--- api/views.py
+++ api/views.py
@@ -76,9 +76,81 @@ class SingleCategoryRecipe(generics.RetrieveUpdateDestroyAPIView):
 class RecipesViewSet(viewsets.ModelViewSet):
     
     def get_queryset(self):
-        queryset = Recipe.objects.all().filter(owner=self.request.user)
+        queryset = Recipe.objects.all().filter(owner=self.request.user)      
         return queryset
     serializer_class = RecipeSerializer
 
+    # Only authenticated users can create recipes
+    def create(self, request, *args, **kwargs):
+        if request.user.is_anonymous:
+            raise PermissionDenied(
+                "Only logged in users with accounts can create recipes")
+        return super().create(request, *args, **kwargs)
+    
+    # user can only delete recipe he created
+    def destroy(self, request, *args, **kwargs):
+        recipe  = Recipe.objects.get(pk=self.kwargs["pk"])
+        if not request.user == recipe.owner:
+            raise PermissionDenied(
+                "You have no permissions to delete this recipe")
+        return super().destroy(request, *args, **kwargs)
+    
+    # user can only delete category he created
+    def update(self, request, *args, **kwargs):
+        recipe  = Recipe.objects.get(pk=self.kwargs["pk"])
+        if not request.user == recipe.owner:
+            raise PermissionDenied(
+                "You have no permissions to edit this recipe")
+        return super().update(request, *args, **kwargs)
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class PublicRecipes(generics.ListAPIView):
+    
+    permission_classes = ()
+    
+    def get_queryset(self):
+        queryset = Recipe.objects.all().filter(is_public=True)
+        return queryset
+    serializer_class = RecipeSerializer
+
+
+class PublicRecipesDetail(generics.RetrieveAPIView):
+    permission_classes = ()
+    
+    def get_queryset(self):
+        queryset = Recipe.objects.all().filter(is_public=True)
+        return queryset
+    serializer_class = RecipeSerializer
+    
+
+class RecipeReviews(viewsets.ModelViewSet):
+    
+    permission_classes = ()
+    serializer_class = ReviewSerialiZer
+    
+    def get_queryset(self):
+        queryset = Review.objects.all().filter(recipe=self.kwargs['pk'])
+        return queryset
+
+
+class UpVoteViewSet(viewsets.ModelViewSet):
+    
+    serializer_class = UpvoteSerializer
+
+    def get_queryset(self):
+        queryset = Upvote.objects.all().filter(recipe=self.kwargs['pk'])
+        return queryset
+    
+    # A user can only upvote a recipe once
+    def create(self, request, *args, **kwargs):
+        upvote = Upvote.objects.filter(recipe=self.kwargs['pk']).first()
+        if upvote and request.user == upvote.voted_by:
+            raise PermissionDenied(
+                "You can not vote for recipe more than once")
+        return super().create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        serializer.save(voted_by=self.request.user)
