82e61ddc81c47dddc9bd69bee5bd7f0a917065f5
PatrickCmd <pwalukagga@gmail.com>
2018-08-14 16:26:28 +0300

    Enable whitenoise

--- yummy_recipes/settings.py
+++ yummy_recipes/settings.py
@@ -185,6 +185,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
