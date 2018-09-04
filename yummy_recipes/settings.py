3df9a9aeafe51615955f3a484f24274d5bf0bcc8
PatrickCmd <pwalukagga@gmail.com>
2018-09-04 14:10:11 +0300

    [chore tests and coverage] change test command from using nosetests

--- yummy_recipes/settings.py
+++ yummy_recipes/settings.py
@@ -162,14 +162,14 @@ JWT_AUTH = {
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-NOSE_ARGS = [
-    '--cover-erase',
-    '-v',
-    '--with-coverage',
-    '--cover-package=authentication,api',
-    '--cover-html',
-]
+# Tell nose to measure coverage on the 'authentication' and 'api' apps
+# NOSE_ARGS = [
+#     '--cover-erase',
+#     '-v',
+#     '--with-coverage',
+#     '--cover-package=authentication,api',
+#     '--cover-html',
+# ]
 
 
 # Internationalization
