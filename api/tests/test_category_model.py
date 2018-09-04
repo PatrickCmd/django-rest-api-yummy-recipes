1694e8b2c5f720ce385ef3990d9fe0371cd91394
PatrickCmd <pwalukagga@gmail.com>
2018-09-04 11:30:56 +0300

    [chore tests and coverage] test category creation

--- api/tests/test_category_model.py
+++ api/tests/test_category_model.py
@@ -1,9 +1,28 @@
 from django.test import TestCase
 
 from api.models import Category
+from authentication.models import User
 
 
 class TestCategoryModel(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name="test",
+            last_name="user",
+            username='test1',
+            email='test1@test.com',
+            password='test12345'
+        )
+
+        self.category = Category.objects.create(
+            name='test category',
+            owner=self.user,
+            description='sweet and flavor category'
+        )
+    
+    def test_category_name(self):
+        self.assertEqual(self.category.name, 'test category')
 
     def test_string_representation(self):
         '''
